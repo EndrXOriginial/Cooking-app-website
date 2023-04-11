ffeb45083f072ac4f3de9fd874b72f52519692c0
EndrX <chris.pin@hotmail.ca>
2023-04-11 11:26:43 -0400

    finished the nav component and css

--- src/Components/NavBar/navbar.js
+++ src/Components/NavBar/navbar.js
@@ -1,14 +1,17 @@
-import  './navbar.css';
+import './navbar.css';
 
 export const Navbar = () => {
     return (
-        <nav className='nav-bar'>
-            <ul>
-                <li><a>Home</a></li>
-                <li><a>How it works</a></li>
-                <li><a>About the app</a></li>
-                <li><a>Download</a></li>
-            </ul>
+        <nav className='navbar'>
+            <div className='wrapper nav'>
+                <img src='../assets/images/logo.svg' alt='Logo of the application' />
+                <ul>
+                    <li><a className='nav-link'>Home</a></li>
+                    <li><a className='nav-link'>How it works</a></li>
+                    <li><a className='nav-link'>About the app</a></li>
+                    <li><a className='nav-link'>Download</a></li>
+                </ul>
+            </div>
         </nav>
     )
 }
